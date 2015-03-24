2747f570938b47aee752ba4b5c7ee7403aedde06
Naohisa Orita <ori534@gmail.com>
2015-03-25 01:32:32 +0900

    env test

--- fuel/app/config/test/db.php
+++ fuel/app/config/test/db.php
@@ -9,8 +9,7 @@ return array(
 	'default' => array(
 		'connection'  => array(
 			'dsn'        => 'mysql:host=localhost;dbname=fuel_test',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
+			'username'   => 'root',
 		),
 	),
 );
